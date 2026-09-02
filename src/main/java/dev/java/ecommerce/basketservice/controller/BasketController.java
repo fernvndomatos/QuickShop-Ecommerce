4dadbf85254e463efb7a1beb220b3163a3aed5d6
Fernando Matos <fernando.matos221@gmail.com>
2026-09-02 14:16:46 -0300

    fix: correct PUT mapping path from /id to /{id} in BasketController

--- src/main/java/dev/java/ecommerce/basketservice/controller/BasketController.java
+++ src/main/java/dev/java/ecommerce/basketservice/controller/BasketController.java
@@ -25,7 +25,7 @@ public class BasketController {
         return ResponseEntity.status(HttpStatus.CREATED).body(basketService.createBasket(request));
     }
 
-    @PutMapping("/id")
+    @PutMapping("/{id}")
     public ResponseEntity<Basket> updateBasket(@PathVariable String id, @RequestBody BasketRequest request){
         return ResponseEntity.status(HttpStatus.OK).body(basketService.updateBasket(id, request));
     }
